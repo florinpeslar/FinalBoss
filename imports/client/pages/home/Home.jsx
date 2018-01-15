4051a761f47370c5cf7ae994758ec79faeff4d5c
Florin Peslar <florin@code-alchemy.com>
2018-01-15 12:41:44 +0200

    twbs package added, home.jsx style changes

--- imports/client/pages/home/Home.jsx
+++ imports/client/pages/home/Home.jsx
@@ -3,11 +3,37 @@ import React, {Component} from 'react';
 class Home extends Component {
     render() {
         return (
-            <main>
-                Welcome!
-                <a href={FlowRouter.url('login')}>Login</a>
-                <a href={FlowRouter.url('register')}>Register</a>
-            </main>
+            <div>
+                <main className="navbar navbar-default">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="/">Donuts!</a>
+                        </div>
+
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><a href={FlowRouter.url('login')}>Login</a></li>
+                                <li><a href={FlowRouter.url('register')}>Register</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </main>
+
+                <div className="jumbotron">
+                    <div className="container">
+                        <h1 className="text-center">Hello Donut!</h1>
+                    </div>
+                </div>
+            </div>
+
         )
     }
 }
